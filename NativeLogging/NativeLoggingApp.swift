8bdd5fd85dc4b208d59f374d0e6b8e594e1afd72
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-12-22 14:15:12 -0300

    first commit

---
+++ NativeLogging/NativeLoggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeLoggingApp.swift
+//  NativeLogging
+//
+//  Created by thaxz on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NativeLoggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
