de03c516e7dc4dc4508615c53400b984e0ce3972
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-12-26 15:27:25 -0300

    feature(Debug): Adding debug statements

--- NativeLogging/Views/ContentView.swift
+++ NativeLogging/Views/ContentView.swift
@@ -11,20 +11,25 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 20){
             Button("Add info entry") {
-                Logger.info("This is an info entry")
+                Logger.info("This is an info entry with some JSON response")
             }
             .buttonStyle(.borderedProminent)
             .tint(.blue)
             Button("Add warning entry") {
-                Logger.warning("This is a warning")
+                Logger.warning("This is a warning entry with some JSON response")
             }
             .buttonStyle(.borderedProminent)
             .tint(.yellow)
             Button("Add error entry") {
-                Logger.error("This is an error")
+                Logger.error("This is an error entry with some JSON response")
             }
             .buttonStyle(.borderedProminent)
             .tint(.red)
+            Spacer()
+            Button("SEND LOG") {
+                // Calls ViewModel upload function
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
